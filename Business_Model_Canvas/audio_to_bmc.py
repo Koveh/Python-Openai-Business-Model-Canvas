940654c194b8a09cb65d51ebcbba8c8e533f1260
Koveh <h12014584@s.wu.ac.at>
2023-04-09 23:27:27 +0200

    new version

--- Business_Model_Canvas/audio_to_bmc.py
+++ Business_Model_Canvas/audio_to_bmc.py
@@ -1,9 +1,10 @@
+import ast
 import openai
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder='templates')
 
-openai.api_key = "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"
+openai.api_key = "sk-NCl9ebUEbOu1T3BlbkFJ1peagkXsv2N2bOuvr1RI"
 
 def generate_bmc_dictionary():
     with open("audio2.m4a", "rb") as audio_file:
@@ -12,24 +13,26 @@ def generate_bmc_dictionary():
     text = transcript.text
     print (text)
 
+    
     prompt = (
         f"take the given text:\n\n{text}\n\n"
-        f"make a business model canvas based on the ideas that is described above. Give as an output: python dictionary format with following keys and values as the list of strings with different example for each of business model canvas element: \n"
-        f'dict("key_partners": ["___", "___"], "key_activities": ["___", "___"], "key_resources": ["___", "___"], "value_propositions": ["___", "___"], "customer_relationships": ["___", "___"], "channels": ["___", "___"], "customer_segments": ["___", "___"], "cost_structure": ["___", "___"], "revenue_streams": ["___", "___"])'
+        "make a business model canvas based on the ideas that is described above. Give as an output: python dictionary format with following keys and values as the list of strings with precise 4-10 word description for each of business model canvas element: \n"
+        '{"key_partners": ["___",  "___", "___"], "key_activities": ["___",  "___", "___"], "key_resources": ["___",  "___", "___"], "value_propositions": ["___",  "___", "___"], "customer_relationships": ["___",  "___", "___"], "channels": ["___",  "___", "___"], "customer_segments": ["___",  "___", "___"], "cost_structure": ["___",  "___", "___"], "revenue_streams": ["___",  "___", "___"]})'
     )
 
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        max_tokens=4097,
+        max_tokens=3500,
         n=1,
         stop=None,
         temperature=0.5,
     )
 
-    bmc_dictionary = eval(response.choices[0].text.strip())
+    bmc_dictionary = ast.literal_eval(response.choices[0].text.strip())
     print(bmc_dictionary)
     return bmc_dictionary
+
 
 
 @app.route('/')
--- Business_Model_Canvas/text_to_bmc.py
+++ Business_Model_Canvas/text_to_bmc.py
@@ -1,10 +1,11 @@
+import ast
 import openai
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder='templates')
 
 
-openai.api_key = "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"
+openai.api_key = "sk-NCl9ebUEbOu1T3BlbkFJ1peagkXsv2N2bOuvr1RI"
 
 # insert your text here
 text = '''Well, if I were to bring this idea to life, I think there could be a significant demand for a password-keeping watch, especially among individuals who value security and convenience. I could potentially market this watch to professionals, entrepreneurs, and other individuals who need to manage a lot of passwords for work or personal use.
@@ -14,35 +15,35 @@ In terms of pricing, I would have to consider the costs of manufacturing, market
 I think the key to success would be a strong marketing strategy, particularly in the digital space. I would need to create a buzz around the product and highlight its unique features to stand out from other smartwatches on the market. I would also need to partner with key influencers and thought leaders to promote the product and generate interest.
 
 In terms of costs, I would need to factor in the costs of research and development, manufacturing, marketing, and distribution. I would also need to consider the costs of ongoing support and maintenance for the product.
-
-Overall, while there may be some challenges associated with bringing this product to market, I think it has the potential to be a profitable and innovative solution to a common problem. With the right strategy and execution, I believe this password-keeping watch could be a success.
 '''
 
 def generate_bmc_dictionary(text):
 
     prompt = (
         f"take the given text:\n\n{text}\n\n"
-        f"make a business model canvas based on the ideas that is described above. Give as an output: python dictionary format with following keys and values as the list of strings with different example for each of business model canvas element: \n"
-        f'dict("key_partners": ["___", "___"], "key_activities": ["___", "___"], "key_resources": ["___", "___"], "value_propositions": ["___", "___"], "customer_relationships": ["___", "___"], "channels": ["___", "___"], "customer_segments": ["___", "___"], "cost_structure": ["___", "___"], "revenue_streams": ["___", "___"])'
+        "Make a Business Model Canvas based on the text above. Give as an output: python dictionary format with following keys and values as the list of strings. Each point ___ must have 3-10 word description: \n"
+        '{"key_partners": ["___",  "___", "___"], "key_activities": ["___",  "___", "___"], "key_resources": ["___",  "___", "___"], "value_propositions": ["___",  "___", "___"], "customer_relationships": ["___",  "___", "___"], "channels": ["___",  "___", "___"], "customer_segments": ["___",  "___", "___"], "cost_structure": ["___",  "___", "___"], "revenue_streams": ["___",  "___", "___"]})'
     )
 
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
-        max_tokens=4097,
+        max_tokens=3500,
         n=1,
         stop=None,
         temperature=0.5,
     )
 
-    bmc_dictionary = eval(response.choices[0].text.strip())
+    bmc_dictionary = ast.literal_eval(response.choices[0].text.strip())
     print(bmc_dictionary)
     return bmc_dictionary
+
 
 @app.route('/')
 def bmc():
     bmc_data = generate_bmc_dictionary(text)
     return render_template('bmc.html', bmc=bmc_data)
+    
 
 
 if __name__ == '__main__':
